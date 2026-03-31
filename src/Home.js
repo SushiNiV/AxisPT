4c52ed1f04e472e67dd7da8fc8faf3e0254d1a96
SushiNiV <sha.gmdev@gmail.com>
2026-03-31 17:52:24 +0800

    Initial Upload

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Home.css';
+import cptLogo from './assets/cpt-logo.png'; 
+
+function Home() {
+  return (
+    <div className="home-container"> 
+        <div className='topBar'>
+            <div className='logoArea'>
+                <img src={cptLogo} alt='CPT Logo' className='mainLogo'/>
+                <p className='mainTitle'>College of Physical Therapy</p>
+            </div>
+            <div className='searchArea'>
+                <div className='searchBar'>
+                    <input type='text' placeholder='Search...' className='searchInput'/>
+                    <button className='searchButton'>
+                        <span className="material-icons">search</span>
+                    </button>
+                </div>
+                <div className='enrollBar'>
+                    <button className='enrollBT'>
+                        Enroll Now!
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div className='navBar'>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
